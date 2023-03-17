8fba3272d1304ee3f0b70262d54bf7a163a7fa4f
PizzasaurusR <ramizissel@gmail.com>
2023-03-17 05:42:53 -0700

    Create 6-print_numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print numbers base 10 form 0 to 9
+ * and then new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar("\n");
+	return (0);
+}
