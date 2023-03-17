52ed139b3af716e0904ffb67074b9113635fc4bc
PizzasaurusR <ramizissel@gmail.com>
2023-03-17 07:11:02 -0700

    Create 100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible 2-digit combos
+ * digits must be different
+ * print in ascending order
+ * only use putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int firstD, secD;
+
+	for (firstD = 48; n <= 56; firstD++)
+	{
+		for (secD = 49; secD <= 57; secD++)
+		{
+			if (secD > firstD)
+			{
+				putchar(firstD);
+				putchar(secD);
+				if (firstD != 56 || secD != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
