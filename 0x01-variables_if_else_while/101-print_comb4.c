11fdcc093822245c0625b24a91620c191ccfa9af
PizzasaurusR <ramizissel@gmail.com>
2023-03-17 07:28:42 -0700

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combos of three digits
+ * digits must be different
+ * only smalest combos
+ * only putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int firstD, secD, thirD;
+	{
+		for (firstD = 48, firstD < 58; firstD++)
+		{
+			for (secD = 49; secD < 58; secD++)
+			{
+				for (thirD = 50; thirdD < 58; thirdD++)
+				{
+					if (thirD > secD && secD > firstD)
+					{
+						putchar(firstD);
+						putchar(secD);
+						putchar(thirD);
+						if (firstD != 55 || secD != 56)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
