4a2de80fe6f4c809c79711a1180613c9085305a2
PizzasaurusR <ramizissel@gmail.com>
2023-03-21 16:16:57 -0700

    Create 9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 time table
+ * but actually prints all the times tables
+ * it weird I know
+ * what you gonna do?
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int row, col, mult, ten, one;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			mult = row * col;
+			ten = mult / 10;
+			one = mult % 10;
+
+			if (col == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(one + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(ten + '0');
+				_putchar(one + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
