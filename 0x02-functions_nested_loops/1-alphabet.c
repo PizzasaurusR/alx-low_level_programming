d03078d11cdb113ec885d785ac59c8c36ec6dbf9
PizzasaurusR <ramizissel@gmail.com>
2023-03-21 14:17:08 -0700

    Create 1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * main - print the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char l = 'a';
+	while (l <= 'z')
+	{
+		_putchar(l);
+		l++;
+	}
+	_putchar('\n');
+}
