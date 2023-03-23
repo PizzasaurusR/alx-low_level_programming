fd60afb639dd47bd90368719306b0dff3706d320
PizzasaurusR <ramizissel@gmail.com>
2023-03-23 15:16:09 -0700

    Wahtever

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,20 +7,16 @@
 
 void more_numbers(void)
 {
-	int i, j, k;
+	int i, j;
 
-	for (j = 1; i <= 10; ++j)
+	for (i = 0; i < 10; i++)
 	{
-		for (k = 0; k <= 14; ++k)
+		for (j = 0; j < 15; j++)
 		{
-			i = k;
-			if (k >= 10)
-			{
-				_putchar(1 + '0');
-				i = k % 10;
-			}
-			_putchar(i + '0');
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
