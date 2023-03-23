5a3732847ef2bf1ab5e81dd18e3aff2e490abeeb
PizzasaurusR <ramizissel@gmail.com>
2023-03-23 14:38:24 -0700

    Create 3-pitn_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-prints_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0-9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
