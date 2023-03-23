094ca1632909567bbc9873d77e9e8d3a5b4ef89e
PizzasaurusR <ramizissel@gmail.com>
2023-03-23 14:56:30 -0700

    I really don't like betty

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - prints 0-9
+ * print_most_numbers - prints 0-9
  * but not 2 and 4
  * then new line
  */
@@ -12,10 +12,9 @@ void print_most_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		if (i != 2 && i != 4)
-		{
-			_putchar(i + '0');
-		}
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + '0');
 	}
 
 	_putchar('\n');
