ed832175b6c31bf7e37683f22874ad644431c75f
PizzasaurusR <ramizissel@gmail.com>
2023-04-09 15:42:18 -0700

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * mynameis - print name of program
+ * @argc - num arguments
+ * @argv - argument arr
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mynameis(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
