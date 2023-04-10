444c7e2cae54cd8d3bfed5bc3ba7e537f0ad4691
PizzasaurusR <ramizissel@gmail.com>
2023-04-09 17:48:19 -0700

    create 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - print min num coins to 
+ * give exact change
+ * @argc: num arguments
+ * @argv: argument arr
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+
+int main( int argc, char *argv[])
+{
+	int cents, numCoins,  change;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		cents = atoi(argv[1]);
+
+		if (cents < 0)
+		{
+			printf("0\n");
+			return (0);
+		}
+
+		for (numCoins = 0; numCoins < 5 && cents >= 0; numCoins++)
+		{
+			while (cents >= coins[numCoins])
+			{
+				change++;
+				cents -= coins[numCoins];
+			}
+		}
+
+		printf("%d\n", change);
+		return (0);
+	}
+}
