22ed1a77fc68a7cdecd42b87113b67546c2477f3
PizzasaurusR <ramizissel@gmail.com>
2023-04-11 17:57:11 -0700

    Create 100-

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr -concat all arguments
+ *
+ * @ac: argument counter
+ * @av: argument holder
+ *
+ * Return: pointer to new str
+ *         or NULL if error
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int x, y, z, len;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	len = 0;
+
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y] != '\0'; y++)
+		{
+			len++;
+		}
+		len++;
+	}
+
+	s = malloc((len + 1) * sizeof(char));
+	
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	z = 0;
+	
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y] != '\0'; y++)
+		{
+			s[z] = av[x][y];
+			z++;
+		}
+		s[z] = '\n';
+		z++;
+	}
+	s[z] = '\0';
+
+	return (s);
+}
