956f8f87fdb8ce647979c198d713412511098b93
PizzasaurusR <ramizissel@gmail.com>
2023-04-13 14:39:29 -0700

    Made the code smaller

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,43 +18,28 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int x;
 
 	if (new_size == old_size)
-	{
 		return (ptr);
-	}
-
 	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	if (!ptr)
-	{
 		return (malloc(new_size));
-	}
-
 	ptr1 = malloc(new_size);
-
 	if (!pntr1)
-	{
 		return (NULL);
-	}
-
 	pntr2 = ptr;
-
 	if (new_size < old_size)
 	{
 		for (x = 0; x < new_size; x++)
 			pntr1[x] = pntr2[x];
 	}
-
 	if (new_size > old_size)
 	{
 		for (x = 0; x < old_size; x++)
 			pntr1[x] = pntr2[x];
 	}
-
 	free(ptr);
-
 	return (pntr1);
 }
