275a914bf4a2ca01fdac6460efe4055b5ad45737
PizzasaurusR <ramizissel@gmail.com>
2023-04-19 05:03:39 -0700

    Create 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute func given as param
+ * for each arr element
+ * @array: arr to iterate
+ * @size: arr size
+ * @action: pntr to func
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
